2dc1b54d65edc588f78da586b5bc1e8f2b864cc3
Alvin <alvinamuiru2022@gmail.com>
2023-05-26 19:17:28 +0300

    Adding a hamburger icon with a dropdown menu

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html',"./build/js/*.js"],
+  theme: {
+    extend: {
+    keyframes: {
+     'open-menu': {
+      '0%' : {transform: 'scaleY(0)'},
+      '80%' : {transform: 'scaleY(1.2)'},
+      '100%' : {transform: 'scaleY(1)'}, 
+    },
+    }, 
+   
+     animation: {
+      'open-menu': 'open-menu 1.5s ease-in-out forwards',
+    }
+
+  
+    },
+  },
+  plugins: [],
+}
+
